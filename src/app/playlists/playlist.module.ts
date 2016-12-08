aafb8494bd9d91cb7162afd947c0b64c39907211
Khoa Nguyen Dang <ndkhoa1@tma.com.vn>
2016-12-08 17:56:37 +0700

    Import JsonpModule in Playlist moduled. Cause Playlist component uses Song service, and song service uses JsonpModule.

--- src/app/playlists/playlist.module.ts
+++ src/app/playlists/playlist.module.ts
@@ -4,12 +4,14 @@ import { PlaylistsComponent } from "./playlists.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { PlaylistFormComponent } from "./playlist-form.component";
+import { JsonpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     PlaylistRoutingModule,
     CommonModule,
     FormsModule,
+    JsonpModule,
     ReactiveFormsModule
   ],
   exports: [],
